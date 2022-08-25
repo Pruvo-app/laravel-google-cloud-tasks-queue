9b78bed6d097bbc1c1380e38e19fd32481a27f77
Ennio Sousa <ennio21@gmail.com>
2022-08-25 15:38:33 -0300

    Update env name

--- config/cloud-tasks.php
+++ config/cloud-tasks.php
@@ -4,7 +4,7 @@ use Pruvo\LaravelGoogleCloudTasksQueue\TaskHandler;
 
 return [
     'handler'=> [
-        'uri'=> env('CLOUD_TASKS_URI', '/handle-task'),
+        'uri'=> env('CLOUD_TASKS_HANDLER_URI', '/handle-task'),
         'controller'=> TaskHandler::class,
         'name'=> 'cloud-tasks.handle-task',
     ],
